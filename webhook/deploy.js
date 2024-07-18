4f7b5bd43ec14b855af9ff914811973f2494613e
Aleksandr Istomin <istalexnik@gmail.com>
2024-07-18 02:29:51 -0400

    Fix deployment script to correctly change working directory

--- webhook/deploy.js
+++ webhook/deploy.js
@@ -1,17 +1,17 @@
 const { exec } = require('child_process');
 const fs = require('fs');
+const path = require('path');
 
-const logFile = 'D:\\Projects\\logs\\webhook\\exec_output_log.txt';
-const errorLogFile = 'D:\\Projects\\logs\\webhook\\exec_error_log.txt';
-const debugLogFile = 'D:\\Projects\\logs\\webhook\\exec_debug_log.txt';
-const deployDebugLogFile = 'D:\\Projects\\logs\\webhook\\deploy_debug_log.txt';
+const logFile = path.join('D:', 'Projects', 'logs', 'webhook', 'exec_output_log.txt');
+const errorLogFile = path.join('D:', 'Projects', 'logs', 'webhook', 'exec_error_log.txt');
+const debugLogFile = path.join('D:', 'Projects', 'logs', 'webhook', 'exec_debug_log.txt');
+const deployDebugLogFile = path.join('D:', 'Projects', 'logs', 'webhook', 'deploy_debug_log.txt');
 
 const commands = [
   'git config --global --add safe.directory D:/Projects',
-  'cd D:\\Projects\\webhook',
   'git pull origin main',
   'npm install',
-  'pm2 reload D:\\Projects\\ecosystem.config.js --env production'
+  'pm2 reload D:/Projects/ecosystem.config.js --env production'
 ];
 
 fs.appendFileSync(debugLogFile, 'Starting deploy script...\n', 'utf8');
